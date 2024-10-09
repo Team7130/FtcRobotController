ef9c1b58ef92d48137b0b1f48c0f284c2d26dc5e
Team7130 <srepko@regisroyals.org>
2024-10-09 07:52:35 -0500

    Probably got everything working

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFIRSTJavaOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFIRSTJavaOpMode.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.hardware.lynx.LynxServoController;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -12,17 +13,17 @@ import org.opencv.videoio.Videoio;
 
 @TeleOp(group = "Iterative OpMode", name = "Basic: Iterative OpMode")
 public class MyFIRSTJavaOpMode extends OpMode {
-    private Servo airplaneServo = null;
-    double airplaneServoPos = 0.0d;
-    int airplaneTimerDelay = Videoio.CAP_QT;
-    long airplaneTimerMillis = System.currentTimeMillis();
-    private Servo hand = null;
+    private CRServo hand = null;
+    private Servo wrist = null;
     double handpos = 0.0d;
+    double wristpos = 0.0d;
     private DcMotor leftDriveBack = null;
     private DcMotor leftDriveFront = null;
     private DcMotor rightDrive = null;
     private DcMotor rightDriveBack = null;
     private DcMotor rightDriveFront = null;
+
+    private DcMotor arm = null;
     private ElapsedTime runtime = new ElapsedTime();
     private double shldIntegral = 0.0d;
     private double shldsetpoint = -50.0d;
@@ -30,23 +31,26 @@ public class MyFIRSTJavaOpMode extends OpMode {
 
     public void init() {
         this.telemetry.addData("Status", (Object) "Initialized");
-        this.leftDriveFront = (DcMotor) this.hardwareMap.get(DcMotor.class, "frontleftmotor");
-        this.leftDriveBack = (DcMotor) this.hardwareMap.get(DcMotor.class, "backleftmotor");
-        this.rightDriveFront = (DcMotor) this.hardwareMap.get(DcMotor.class, "frontrightmotor");
-        this.rightDriveBack = (DcMotor) this.hardwareMap.get(DcMotor.class, "backrightmotor");
-        Servo servo = (Servo) this.hardwareMap.get(Servo.class, "plane");
-        this.airplaneServo = servo;
-        servo.setDirection(Servo.Direction.FORWARD);
+        this.leftDriveFront = (DcMotor) this.hardwareMap.get(DcMotor.class, "FLMotor");
+        this.leftDriveBack = (DcMotor) this.hardwareMap.get(DcMotor.class, "BLMotor");
+        this.rightDriveFront = (DcMotor) this.hardwareMap.get(DcMotor.class, "FRMotor");
+        this.rightDriveBack = (DcMotor) this.hardwareMap.get(DcMotor.class, "BRMotor");
         this.shoulder = (DcMotor) this.hardwareMap.get(DcMotor.class, "shoulder");
-        Servo servo2 = (Servo) this.hardwareMap.get(Servo.class, "hand");
-        this.hand = servo2;
-        servo2.setDirection(Servo.Direction.FORWARD);
+        this.arm = (DcMotor) this.hardwareMap.get(DcMotor.class, "arm");
+
+        this.hand = (CRServo) this.hardwareMap.get(CRServo.class, "hand");
+
+        Servo servo3 = (Servo) this.hardwareMap.get(Servo.class, "wrist");
+        this.wrist = servo3;
+        servo3.setDirection(Servo.Direction.FORWARD);
+
         this.leftDriveFront.setDirection(DcMotorSimple.Direction.FORWARD);
         this.leftDriveBack.setDirection(DcMotorSimple.Direction.FORWARD);
         this.rightDriveFront.setDirection(DcMotorSimple.Direction.FORWARD);
         this.rightDriveBack.setDirection(DcMotorSimple.Direction.FORWARD);
         this.shoulder.setDirection(DcMotorSimple.Direction.REVERSE);
         this.handpos = 0.0d;
+        this.wristpos = 0.0d;
         double currentPosition = (double) this.shoulder.getCurrentPosition();
         this.shldsetpoint = currentPosition;
         this.shldIntegral = currentPosition;
@@ -68,24 +72,29 @@ public class MyFIRSTJavaOpMode extends OpMode {
         double rotateShld = (double) this.gamepad2.right_stick_y;
         double openHand = (double) this.gamepad2.left_trigger;
         double closeHand = (double) this.gamepad2.right_trigger;
-        double airplaneTrg = (double) this.gamepad1.left_trigger;
+        boolean wristUp = this.gamepad2.dpad_up;
+        boolean wristDown = this.gamepad2.dpad_down;
+        double armVal = (double) this.gamepad2.left_stick_y;
         double rotateShld2 = rotateShld;
         if (openHand > 0.0d) {
-            this.handpos = 1.0d;
+            this.hand.setPower(1.0d);
         }
         if (closeHand > 0.0d) {
             this.handpos = 0.0d;
+            this.hand.setPower(-1.0d);
         }
-        this.hand.setPosition(this.handpos);
-        if (airplaneTrg > 0.0d) {
-            this.airplaneServoPos += 0.25d;
-            double d = airplaneTrg;
-            this.airplaneTimerMillis = System.currentTimeMillis() + ((long) this.airplaneTimerDelay);
+        else {
+            this.hand.setPower(0.0);
         }
-        if (System.currentTimeMillis() > this.airplaneTimerMillis) {
-            this.airplaneServoPos = 0.0d;
+
+        if(wristUp) {
+            this.wristpos = 0.6d;
+            this.wrist.setPosition(wristpos);
+        } else if(wristDown) {
+            this.wristpos = 0.0d;
+            this.wrist.setPosition(wristpos);
         }
-        this.airplaneServo.setPosition(this.airplaneServoPos);
+
         if (strafe < 0.1d && strafe > -0.1d) {
             strafe = 0.0d;
         }
@@ -95,24 +104,27 @@ public class MyFIRSTJavaOpMode extends OpMode {
         if (turn < 0.1d && turn > -0.1d) {
             turn = 0.0d;
         }
+
+        if(Math.abs(armVal) > 0.1) {
+            this.arm.setPower(armVal);
+        } else {
+            this.arm.setPower(0.0d);
+        }
+        
         double leftDrivePower = Range.clip(drive - turn, -5.0d, 5.0d);
         double rightDrivePower = Range.clip(drive + turn, -5.0d, 5.0d);
         double strafePower = Range.clip(strafe, -5.0d, 5.0d);
-        double d2 = closeHand;
-        double d3 = openHand;
         double shoulderPower = Range.clip(rotateShld2, -1.0d, 1.0d);
         this.leftDriveFront.setPower(leftDrivePower - strafePower);
         this.leftDriveBack.setPower(leftDrivePower + strafePower);
         this.rightDriveFront.setPower(rightDrivePower - strafePower);
         this.rightDriveBack.setPower(rightDrivePower + strafePower);
-        double handposTemp = this.hand.getPosition();
         this.shoulder.setPower(shoulderPower);
         double d4 = strafe;
         this.telemetry.addData("Status", (Object) "Run Time: " + this.runtime.toString());
         double d5 = drive;
         this.telemetry.addData("Drive Motors", "left (%.2f), right (%.2f)", Double.valueOf(leftDrivePower), Double.valueOf(rightDrivePower));
         this.telemetry.addData("Arm Motor", "shoulder (%.2f)", Double.valueOf((double) this.shoulder.getCurrentPosition()));
-        this.telemetry.addData("handpos", "hand (%.2f)", Double.valueOf(handposTemp));
     }
 
     public void stop() {
